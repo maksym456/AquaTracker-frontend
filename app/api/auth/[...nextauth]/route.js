a7c2661d66e101ee1b2a27292258855feb500fbb
Maksym Wilk <maksym.wilk@studenci.collegiumwitelona.pl>
2026-01-25 08:59:25 +0100

    small facebook login fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -17,6 +17,7 @@ const handler = NextAuth({
             clientId: process.env.COGNITO_CLIENT_ID,
             clientSecret: process.env.COGNITO_CLIENT_SECRET,
             issuer: process.env.COGNITO_ISSUER,
+            checks: ["nonce"],
         }),
     ],
     session: { strategy: "jwt" },
